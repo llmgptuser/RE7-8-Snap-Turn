bd069e85e7691968f97783301a814b09654730e0
llmgptuser <llmgptuser@gmail.com>
2026-05-22 16:25:55 -0700

    Update snap_turn_re8.lua

--- snap_turn_re8.lua
+++ snap_turn_re8.lua
@@ -11,9 +11,10 @@ local re8 = require("utility/RE8")
 local cfg = {
     snap_turn_enabled = true,
     snap_turn_back_enabled = true,
-    angle = 45.0,
+    snap_turn_angle = 45.0,
     tilt_threshold = 0.8,
     recenter_threshold = 0.4,
+    smooth_turn_speed = 10.0,
 }
 
 local cfg_path = "re8_vr/snap_turn_config.json"
@@ -66,103 +67,82 @@ end
 
 local is_stick_centered = true
 local is_stick_centered_y = true
-local snap_turn = false
-local snap_turn_sign = 0
 local snap_turn_back = false
-local snap_turn_nonvr = false
-
-re.on_frame(function()
-    if not cfg.snap_turn_enabled then
-        return
-    end
-    if not re8vr.player then
-        return 
-    end
-    if not snap_turn_nonvr and not vrmod:is_hmd_active() then
-        return
-    end
-
-    if is_re7 then
-        local menu_manager = sdk.get_managed_singleton("app.MenuManager")
-        if menu_manager ~= nil then
-            if menu_manager:call("isOpenInventoryMenu") then
-                return
-            end
-            if not menu_manager:call("isCanOpenQuickSlotMenu") then
-                return
-            end                
-        end
-    end
-
-    if is_re8 then
-        local GUIManager = sdk.get_managed_singleton("app.GUIManager")
-        if GUIManager ~= nil then
-            if GUIManager:call("isShowingGUIInventory") then
-                return
-            end
-            if GUIManager:call("isShowingGUIShop") then
-                return
-            end
-        end
-    end
-
-    local right_stick_axis = get_right_input_axis()
-    local x_axis = right_stick_axis.x
-    local y_axis = right_stick_axis.y
-    if is_stick_centered then
-        if math.abs(x_axis) > cfg.tilt_threshold then
-            snap_turn = true
-            snap_turn_sign = math_sign(x_axis)
-            is_stick_centered = false
-        end
-    elseif math.abs(x_axis) < cfg.recenter_threshold then
-        is_stick_centered = true
-    end
-    if cfg.snap_turn_back_enabled and is_stick_centered then
-        if is_stick_centered_y then
-            if y_axis < -cfg.tilt_threshold then
-                snap_turn = true
-                snap_turn_back = true
-                is_stick_centered_y = false
-            end
-        elseif math.abs(y_axis) < cfg.recenter_threshold then
-            is_stick_centered_y = true
-        end
-    end
-end)
 
 sdk.hook(
     sdk.find_type_definition(sdk.game_namespace("PlayerCamera.CameraController")):get_method("calcDeltaHorizontalRotation"),
     function(args)
     end,
     function(retval)
-        if not cfg.snap_turn_enabled or (not snap_turn_nonvr and not vrmod:is_hmd_active()) then
+        if not vrmod:is_hmd_active() or not re8vr.player then
             return retval
         end
-        if snap_turn then
-            snap_turn = false
-            if cfg.snap_turn_back_enabled and snap_turn_back then
-                snap_turn_back = false
-                return sdk.float_to_ptr(180.0)
-            else
-                return sdk.float_to_ptr(cfg.angle * snap_turn_sign)
+
+        if is_re7 then
+            local menu_manager = sdk.get_managed_singleton("app.MenuManager")
+            if menu_manager ~= nil then
+                if menu_manager:call("isOpenInventoryMenu") then
+                    return retval
+                end
+                if not menu_manager:call("isCanOpenQuickSlotMenu") then
+                    return retval
+                end                
+            end
+        end
+
+        if is_re8 then
+            local GUIManager = sdk.get_managed_singleton("app.GUIManager")
+            if GUIManager ~= nil then
+                if GUIManager:call("isShowingGUIInventory") then
+                    return retval
+                end
+                if GUIManager:call("isShowingGUIShop") then
+                    return retval
+                end
+            end
+        end
+
+        local right_stick_axis = get_right_input_axis()
+        local x_axis = right_stick_axis.x
+        local y_axis = right_stick_axis.y
+        if cfg.snap_turn_enabled then
+            if is_stick_centered then
+                if math.abs(x_axis) > cfg.tilt_threshold then
+                    is_stick_centered = false
+                    return sdk.float_to_ptr(math_sign(x_axis) * cfg.snap_turn_angle)
+                end
+            elseif math.abs(x_axis) < cfg.recenter_threshold then
+                is_stick_centered = true
+            end
+            if cfg.snap_turn_back_enabled and is_stick_centered then
+                if is_stick_centered_y then
+                    if y_axis < -cfg.tilt_threshold then
+                        is_stick_centered_y = false
+                        return sdk.float_to_ptr(180.0)
+                    end
+                elseif math.abs(y_axis) < cfg.recenter_threshold then
+                    is_stick_centered_y = true
+                end
             end
         else
-            return sdk.float_to_ptr(0.0)
+            return sdk.float_to_ptr(x_axis * cfg.smooth_turn_speed)
         end
+        return sdk.float_to_ptr(0.0)
     end
 )
 
 
 re.on_draw_ui(function()
     local changed = false
-    if imgui.tree_node("Snap Turn") then
+    if imgui.tree_node("Enhanced Movement") then
         changed, cfg.snap_turn_enabled = imgui.checkbox("Snap Turn Enabled", cfg.snap_turn_enabled)
         if cfg.snap_turn_enabled then
-            changed, cfg.angle = imgui.drag_float("Turn Angle", cfg.angle, 1.0, 15.0, 180.0)
-            changed, cfg.snap_turn_back_enabled = imgui.checkbox("Snap Turn Back Enabled", cfg.snap_turn_back_enabled)
+            changed, cfg.snap_turn_angle = imgui.drag_float("Snap Turn Angle", cfg.snap_turn_angle, 15.0, 15.0, 90.0)
+            changed, cfg.snap_turn_back_enabled = imgui.checkbox("Tild Down to Turn Back Enabled", cfg.snap_turn_back_enabled)
             changed, cfg.tilt_threshold = imgui.drag_float("Snap Turn Tilt Threshold", cfg.tilt_threshold, 0.05, 0.1, 1.0)
             changed, cfg.recenter_threshold = imgui.drag_float("Snap Turn Recenter Threshold", cfg.recenter_threshold, 0.05, 0.1, 1.0)
+        else
+            changed, cfg.smooth_turn_speed = imgui.drag_float("Smooth Turn Speed", cfg.smooth_turn_speed, 1.0, 1.0, 50.0)
         end
         imgui.tree_pop()
     end
